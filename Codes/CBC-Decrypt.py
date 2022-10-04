a8fe58b96d1a4b101323d4d25c0ab5cfea4255c6
Ayham Al-Ali <alali_ayham@yahoo.com>
2022-10-04 23:33:14 +0300

    Update CBC.py

--- Codes/CBC-Decrypt.py
+++ Codes/CBC-Decrypt.py
@@ -2,8 +2,10 @@
 import requests
 import base64
 
+url = "http://mercury.picoctf.net:43275/"
+
 s=requests.Session()
-s.get("http://mercury.picoctf.net:56136/")
+s.get(url)
 cookie=s.cookies["auth_name"]
 print(cookie)
 unb64=base64.b64decode(cookie)
@@ -11,9 +13,10 @@ print(unb64)
 unb64b=base64.b64decode(unb64)
 for i in range (0,128):
    pos=i//8
-   guessdec=unb64b[0:pos]+((unb64b[pos]^(1 &lt;&lt; (i%8))).to_bytes(1,'big'))+unb64b[pos+1:]
+   guessdec=unb64b[0:pos]+((unb64b[pos]^(1 << (i%8))).to_bytes(1,'big'))+unb64b[pos+1:]
    guessenc1 = base64.b64encode(guessdec)
    guess=base64.b64encode(base64.b64encode(guessdec)).decode()
-   r=requests.get("http://mercury.picoctf.net:56136/",cookies={"auth_name": guess})
+   r=requests.get(url, cookies={"auth_name": guess})
    if "pico" in r.text:
        print(r.text)
+       print("guess: " + guess)
